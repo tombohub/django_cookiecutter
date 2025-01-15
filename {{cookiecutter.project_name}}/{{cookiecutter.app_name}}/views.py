95057984abbdbc7d6c7d8d355c79a9b25bc71233
me <tkutlesa@gmail.com>
2025-01-15 00:00:57 -0500

    home url and tempalte

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/views.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request: HttpRequest):
+    return render(request, "{{cookiecutter.app_name}}/home.html")

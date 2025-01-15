16dc4927575426ec28d5b8d5e56ea4ee934e0792
me <tkutlesa@gmail.com>
2025-01-14 23:14:07 -0500

    seceret key

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h5vtx=xvxyrigify))k%y-37zv9bj#(==!s!pa2p@xe=hthmhl"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

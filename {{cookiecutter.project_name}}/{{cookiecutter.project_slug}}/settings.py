83322722d55347d30a79bee4e3eeb4a7a7b970c7
me <tkutlesa@gmail.com>
2025-01-14 23:22:07 -0500

    app added to settings

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # apps
+    "{{cookiecutter.app_name}}",
 ]
 
 MIDDLEWARE = [

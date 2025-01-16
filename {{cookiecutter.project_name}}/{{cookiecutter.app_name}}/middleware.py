5b0edb06043c7b14953ad3a3afeca1e8d9de8229
me <tkutlesa@gmail.com>
2025-01-15 19:11:41 -0500

    superuser  guard

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/middleware.py
@@ -0,0 +1,17 @@
+from typing import Any, Callable
+
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
+class SuperuserRequiredMiddleware:
+    def __init__(self, get_response: Callable):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> Any:
+        if not request.user.is_superuser:  # type: ignore
+            if request.path != reverse("admin:login"):
+                return redirect("admin:login")
+        response = self.get_response(request)
+        return response
--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "{{cookiecutter.app_name}}.middleware.SuperuserRequiredMiddleware"
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
